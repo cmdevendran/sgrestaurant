f6bfdbd36eadafbbc7bff38836e8841df9b5cf43
cmdevendran@gmail.com <cmdevendran@gmail.com>
2017-07-27 11:19:32 +0800

    menuitem fully working

--- src/pages/menuitem/menuitem.ts
+++ src/pages/menuitem/menuitem.ts
@@ -17,52 +17,112 @@ import {AlertController} from 'ionic-angular';
 })
 export class MenuitemPage {
 
-//datas : AskProvider[];
-datas : any[];
+  //datas : AskProvider[];
+  datas: any[];
+  menudatas: any[];
+  restid: string;
+  restname : string;
+  vmenuCat : string;
+
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-  private restData: RestProvider,private alertController: AlertController) {
+    private restData: RestProvider, private alertController: AlertController) {
     this.restData.getUserRestaurants().subscribe(data => {
-      this.datas = data;
-      console.log(this.datas);
+      //  this.datas = data;
+      this.restid = data._id;
+      this.restname = data.name;
+      this.menudatas = data.menucategory;
+      console.log(data);
 
 
-  });
+    });
+
   }
 
-cucumber : string;
+  cucumber: string;
   showAlert(data) {
-  this.cucumber = data.name;
-    console.log(data._id);
+    this.cucumber = data.categoryname;
+
     let alert = this.alertController.create({
 
-        title: this.cucumber,
-        subTitle: 'test',
-        buttons: ['OK']
+      title: this.cucumber,
+      subTitle: data.categoryname,
+      buttons: ['OK']
     });
 
     alert.present();
-}
-
-
-menuCat : any;
-
-mycats : any[];
-
-
-
-  addMenuCategory() {
-
-  let passme = {
-  menuCat : this.menuCat,
   }
-  this.restData.addMenuCategory(passme).subscribe(data => {
-    this.mycats = data;
-    console.log("add menu category"+this.mycats);
 
 
-});
-}
+
+
+  mycats: any[];
+
+
+
+
+
+
+  addMenuCategory(menuCat) {
+
+    console.log(" menucat  : " + menuCat + " id : " + this.restid);
+    let passme = {
+      menuCat1: menuCat,
+      id: this.restid
+    }
+
+    this.restData.addMenuCategory(passme).subscribe(data => {
+      this.menudatas = data.menucategory;
+
+
+
+    });
+    menuCat="";
+  }
+
+  confirmDeleteMenuCategory(menuCate){
+
+      let confirm = this.alertController.create({
+        title: 'Delete Menu Category?',
+        message: 'Confirm to Delete: '+ menuCate,
+        buttons: [
+          {
+            text: 'No',
+            handler: () => {
+              console.log('Disagree clicked');
+            }
+          },
+          {
+            text: 'Yes',
+            handler: () => {
+              console.log('Agree clicked');
+                this.deleteMenuCategory(menuCate);
+            }
+          }
+        ]
+      });
+      confirm.present();
+
+
+
+  }
+
+  deleteMenuCategory(menuCate) {
+
+    console.log(" menucat  : " +menuCate + " id : " + this.restid);
+    let passme = {
+      menuCat1: menuCate,
+      id: this.restid
+    }
+
+    this.restData.deleteMenuCategory(passme).subscribe(data => {
+      this.menudatas = data.menucategory;
+
+
+
+    });
+  }
 
 
 
--- src/providers/restaurant.ts
+++ src/providers/restaurant.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
+import { Http, Headers, HttpModule } from '@angular/http';
 import 'rxjs/add/operator/map';
 import {AuthProvider} from '../providers/auth/auth';
+
 
 
 /*
@@ -24,31 +25,44 @@ export class RestProvider {
 
   // get all Restaurants
   getRestaurants() {
-    return this.http.get('http://localhost:3001/api/restaurant')
+      return this.http.get('http://localhost:3001/api/restaurant')
       .map(res => res.json());
-  }
-
-
-
-  //  get all Restaurants where user is marked with admin access
+      }
+      //  get all Restaurants where user is marked with admin access
 
   getUserRestaurants() {
-
   console.log(this.auth.currentUser);
     return this.http.get('http://localhost:3001/api/restaurant/'+this.auth.currentUser)
       .map(res => res.json());
-  }
+      }
 
 
 
 
 
   addMenuCategory(menuCat) {
-  console.log(this.auth.currentUser);
-  var headers = new Headers();
+    var headers = new Headers();
        headers.append('Content-Type', 'application/json');
-       return this.http.post('http://localhost:3001/api/restaurant/'+this.auth.currentUser, JSON.stringify(menuCat), {headers: headers})
+       return this.http.post('http://localhost:3001/api/restaurant/'+menuCat.id, JSON.stringify(menuCat), {headers: headers})
            .map(res => res.json());
+           }
+
+
+  deleteMenuCategory(menuCat){
+
+  var payload = JSON.stringify(menuCat);
+
+
+  var headers = new Headers();
+     headers.append('Content-Type', 'application/json');
+    // headers.append('Content-Length', Buffer.byteLenght(payload));
+
+
+    // return this.http.delete('http://localhost:3001/api/restaurant/'+menuCat.id).map(res => res.json());
+
+    return this.http.post('http://localhost:3001/api/restaurant/'+menuCat.id+'/'+menuCat.menuCat1, {headers: headers})
+        .map(res => res.json());
+         }
+
 
   }
-}
